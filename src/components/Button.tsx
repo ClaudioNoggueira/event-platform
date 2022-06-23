a5a299378ec3f2303b1450cbc42674b7c887d9ab
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-23 09:57:52 -0300

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
+interface ButtonProps {
+  text: string;
+  href?: string;
+  icon?: string;
+  className?: string;
+}
+
+export default function Button(props: ButtonProps) {
+  const { text, href, className, icon } = props;
+
+  return (
+    <a
+      href={href}
+      className={`p-4 text-sm flex items-center rounded font-bold uppercase gap-2 justify-center cursor-pointer ${className}`}
+    >
+      {icon?.toUpperCase() === "DISCORD" ? <DiscordLogo size={24} /> : ""}
+      {icon?.toUpperCase() === "LIGHTNING" ? <Lightning size={24} /> : ""}
+      {text}
+    </a>
+  );
+}
