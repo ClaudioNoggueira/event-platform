956686861f6a1862298cea08e12928ee98554b0c
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-23 12:06:22 -0300

    Feature(Video component): Get lesson by slug

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,6 +3,35 @@ import { VideoCard } from "./VideoCard";
 import { Button } from "./Button";
 
 import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
 
 interface VideoProps {
   lessonSlug: string;
@@ -10,13 +39,32 @@ interface VideoProps {
 
 export function Video(props: VideoProps) {
   const { lessonSlug } = props;
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: lessonSlug,
+    },
+  });
+
+  if (!data)
+    return (
+      <div className="flex-1">
+        <p>Loading...</p>
+      </div>
+    );
+
+  const {
+    videoId,
+    title,
+    description,
+    teacher: { name: teacherName, bio: teacherBio, avatarURL: teacherAvatar },
+  } = data.lesson;
 
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="Ox_zb2cs9zM" />
+            <Youtube videoId={videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -25,16 +73,8 @@ export function Video(props: VideoProps) {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16 mb-6">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold mb-4">
-              Aula 01 - Abertura do evento Ignite Lab
-            </h1>
-            <p className="text-gray-400 leading-relaxed">
-              Na aula 1 você vai entender quais são as oportunidades que você
-              pode ter se especializando em ReactJS e porque essa é a decisão
-              mais inteligente pra sua carreira no momento! E também vamos dar
-              início ao projeto que vamos desenvolver nessa semana: uma
-              plataforma de conteúdos em vídeo. Bora codar!
-            </p>
+            <h1 className="text-2xl font-bold mb-4">{title}</h1>
+            <p className="text-gray-400 leading-relaxed">{description}</p>
           </div>
           <div className="flex flex-col gap-4">
             <Button
@@ -55,16 +95,12 @@ export function Video(props: VideoProps) {
         <div className="flex items-center gap-4 mb-20">
           <img
             className="h-16 w-16 rounded-full border-2 border-blue-500"
-            src="https://github.com/ClaudioNoggueira.png"
+            src={teacherAvatar}
             alt="Teacher photo"
           />
           <div className="leading-relaxed">
-            <strong className="font-bold text-2xl block">
-              Claudio Vinicius Nogueira
-            </strong>
-            <span className="text-gray-200 text-sm block">
-              Back-End Developer
-            </span>
+            <strong className="font-bold text-2xl block">{teacherName}</strong>
+            <span className="text-gray-200 text-sm block">{teacherBio}</span>
           </div>
         </div>
 
