03e8c972bc29deec7bb07fecfba05373c53d9bb3
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-25 11:14:11 -0300

    Refactor(Video component): Use custom hook 'useGetLessonBySlugQuery'

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,36 +2,8 @@ import { DefaultUi, Player, Youtube } from "@vime/react";
 import { VideoCard } from "./VideoCard";
 import { AnchorButton } from "./AnchorButton";
 
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 import "@vime/core/themes/default.css";
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
 
 interface VideoProps {
   lessonSlug: string;
@@ -39,25 +11,20 @@ interface VideoProps {
 
 export function Video(props: VideoProps) {
   const { lessonSlug } = props;
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: lessonSlug,
     },
   });
 
-  if (!data)
+  if (!data || !data.lesson)
     return (
       <div className="flex-1">
         <p>Loading...</p>
       </div>
     );
 
-  const {
-    videoId,
-    title,
-    description,
-    teacher: { name: teacherName, bio: teacherBio, avatarURL: teacherAvatar },
-  } = data.lesson;
+  const { videoId, title, description, teacher } = data.lesson;
 
   return (
     <div className="flex-1">
@@ -92,17 +59,21 @@ export function Video(props: VideoProps) {
             />
           </div>
         </div>
-        <div className="flex items-center gap-4 mb-20">
-          <img
-            className="h-16 w-16 rounded-full border-2 border-blue-500"
-            src={teacherAvatar}
-            alt="Teacher photo"
-          />
-          <div className="leading-relaxed">
-            <strong className="font-bold text-2xl block">{teacherName}</strong>
-            <span className="text-gray-200 text-sm block">{teacherBio}</span>
+        {teacher && (
+          <div className="flex items-center gap-4 mb-20">
+            <img
+              className="h-16 w-16 rounded-full border-2 border-blue-500"
+              src={teacher.avatarURL}
+              alt="Teacher photo"
+            />
+            <div className="leading-relaxed">
+              <strong className="font-bold text-2xl block">
+                {teacher.name}
+              </strong>
+              <span className="text-gray-200 text-sm block">{teacher.bio}</span>
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="gap-8 grid grid-cols-2">
           <VideoCard
